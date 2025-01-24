146423c20081f3e214aaa48ca91f47cac890b497
vikas yadav <vikkiyadav0106@gmail.com>
2025-01-24 20:59:09 +0530

    sql update

--- sql.sql
+++ sql.sql
@@ -23,23 +23,74 @@ drop table student ;
 select*from student;
 select rollno,name , city from student;
 
-select *from student where grade ="B";
-select *from student where marks > 80;
+select *from student
+where grade ="B";
+select *from student 
+where marks > 80;
+
 select* from student 
 where marks+20>80;
 
-select *from student where grade ="B" and marks > 50;
+select *from student 
+where grade ="B" and marks > 50;
 
-select *from student where grade ="B" or marks > 50;
+select *from student 
+where grade ="B" or marks > 50;
 
-select *from student where marks between 40 and 60;
+select *from student 
+where marks between 40 and 60;
 
-select *from student where city in ("goa" ,"mumbai");
+select *from student 
+where city in ("goa" ,"mumbai");
 
-select *from student where city not in ("goa" ,"mumbai");
+select *from student 
+where city not in ("goa" ,"mumbai");
 
 select *from student limit 2;
 select *from student where grade="a" limit 2;
 
-select *from student order by grade asc; 
-select*from student order by marks desc limit 4;
+select *from student 
+order by grade asc; 
+select*from student 
+order by marks desc limit 4;
+
+select avg(marks)
+from student;
+
+select grade,count(rollno) 
+from student group by grade ; 
+
+select grade,avg(marks) 
+from student group by grade ; 
+
+select city ,avg (marks)
+from student
+group by city order by city ;
+
+select city , count(name)
+from student 
+group by city
+having max(marks)>50 ;
+
+
+select city 
+from student 
+where grade ="B"
+GROUP BY CITY
+HAVING max(MARKS) >50
+;
+
+set sql_safe_updates = 0;
+
+
+update student
+set city ="mumbai"
+where city ="delhi";
+
+update student 
+set grade ="A"
+where marks >=70;
+
+delete from student
+where rollno=15
+
